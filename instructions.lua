7d25c49f71aace096eb8aeb0120fdb76bd4b4038
kermukamu <juho.kerminen@aalto.fi>
2026-01-03 16:41:52 +0200

    Implemented instructions mechanic for enfor enemy be enemy behavior

---
+++ instructions.lua
@@ -0,0 +1,93 @@
+-- Instructions "class"
+local Instructions = {}
+Instructions.__index = Instructions
+
+function Instructions.new(instructionTable, host)
+	local self = setmetatable({}, Instructions)
+	self.instructionTable = instructionTable
+	self.host = host
+	self.ind = 1
+	self.wait = 0
+	return self
+end
+
+function Instructions:printInstructions()
+  print("---- INSTRUCTION TABLE ----")
+
+  if not self.instructionTable or #self.instructionTable == 0 then
+    print("(empty)")
+    print("---------------------------")
+    return
+  end
+
+  for i, line in ipairs(self.instructionTable) do
+    print(string.format("%3d: %s", i, line))
+  end
+
+  print("---------------------------")
+  io.stdout:flush()
+end
+
+function Instructions:readInstruction(instruction)
+	-- Check if nil
+	if not instruction then
+    	self.ind = 1
+    	return false
+  	end
+
+  	-- Parse
+	local parts = {}
+	for part in instruction:gmatch("%S+") do
+		table.insert(parts, part)
+	end
+
+	--- These actions will pass false to caller either due to end or wait
+	if parts[1] == "end" then 
+		self.ind = 1
+		return false
+	end
+
+	if parts[1] == "wait" then
+		self:commandIntegrity(tonumber(parts[2]))
+		self.wait = tonumber(parts[2]) / 1000
+		self.ind = self.ind + 1
+		return false
+	end
+
+	--- These actions will pass true to caller
+	if parts[1] == "goto" then
+		self:commandIntegrity(tonumber(parts[2]))
+		self.ind = tonumber(parts[2])
+		return true
+	end
+
+	if parts[1] == "setXSpeed" then 
+		self:commandIntegrity(tonumber(parts[2]))
+		self.host.xSpeed = tonumber(parts[2])
+		io.stdout:flush()
+	end
+
+	if parts[1] == "shoot" then
+		self.host:shoot(true)
+	end
+
+	self.ind = self.ind + 1
+	return true
+end
+
+function Instructions:commandIntegrity(n)
+	if not n then error("Instruction error at instruction line " .. tostring(self.ind)) end
+end
+
+function Instructions:callInstructions(dt)
+	self.wait = math.max(0, self.wait - dt)
+
+	-- Call readInstruction() for every line until receiving false as return
+	local keepReading = true
+	while self.wait <= 0 and keepReading do
+		keepReading = self:readInstruction(self.instructionTable[self.ind])
+    end
+end
+
+
+return { Instructions = Instructions}
