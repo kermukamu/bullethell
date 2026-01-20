b25ca68b237421b53b763da6d237c1ab8c55c1c6
kermukamu <juho.kerminen@aalto.fi>
2026-01-20 16:39:53 +0200

    Added effectsTable, moved table updates to their own function, and moved enemy spawning to its own function. Added powerup execution on keypress.

--- level.lua
+++ level.lua
@@ -8,6 +8,8 @@ local cinstructions = require("instructions")
 Instructions = cinstructions.Instructions
 local ccool3d = require("cool3d")
 Cool3d = ccool3d.Cool3d
+local cpowerup = require("powerup")
+PowerUp = cpowerup.PowerUp
 
 -- Level "class"
 local Level = {}
@@ -29,6 +31,8 @@ function Level.new(debugmode, host)
 	self.enemyTable = {}
 	self.enemyProjectileTable = {}
 	self.playerProjectileTable = {}
+	self.powerUpTable = {}
+	self.effectsTable = {}
 
 	-- General timer
 	self.timer = 0
@@ -38,6 +42,14 @@ function Level.new(debugmode, host)
 	self.enemySpawnInterval = self.originalSpawnInterval
 	self.enemySpawnCount = 1 -- Actual spawned amount is twice due to mirroring
 	self.spawnTimer = 0
+
+	-- Power up
+	self.powerUpInterval = 5
+	self.powerUpTimer = self.powerUpInterval
+	self.powerUpTextCenterX = love.graphics.getWidth()/2
+	self.powerUpTextY = love.graphics.getHeight() - 100
+	self.powerUpTextScale = 3
+	self.activePowerUp = nil
 
 	-- Countdown for the appearing of ending text
 	self.endingTextWait = 2
@@ -83,80 +95,17 @@ function Level:update(dt)
 
 	-- Check if enemies need to be spawned
 	if self.spawnTimer <= 0 then
-		for i=1, self.enemySpawnCount, 1 do
-
-			-- Spawn off screen
-			local spawnX1 = -50
-			local spawnX2 = love.graphics.getWidth() + 50
-
-			-- Set random y value for spawn location
-			local spawnY1 = math.random(-100, love.graphics.getHeight() / 2)
-			local spawnY2 = math.random(-100, love.graphics.getHeight() / 2)
-
-			-- Geometry
-			local enemyOrientation = 0
-			local enemyScale = 0.8
-			local enemySize = 64
-
-			-- Inititialize enemies
-			local enemyLeft = Enemy.new(spawnX1, spawnY1, enemyOrientation, 
-				enemyScale, enemyScale, enemySize, enemySize, self.enemyInstructions, self)
-			local enemyRight = Enemy.new(spawnX2, spawnY2, enemyOrientation, 
-				enemyScale, enemyScale, enemySize, enemySize, self.enemyInstructions, self)
-
-			-- Treat current position as supposed center and align accordingly
-			enemyLeft:centerToPos()
-			enemyRight:centerToPos()
-
-			-- Mirror directions in instructions
-			enemyLeft.instructions.mag = 1
-			enemyRight.instructions.mag = -1
-
-			-- Set xSpeed multiplier as a random constant between 0.8 and 1.5
-			local xSpeedMultMin = 0.8
-			local xSpeedMultMax = 1.5
-			enemyLeft.xSpeedMult = xSpeedMultMin + math.random() * (xSpeedMultMax - xSpeedMultMin)
-			enemyRight.xSpeedMult = xSpeedMultMin + math.random() * (xSpeedMultMax - xSpeedMultMin)
-
-			-- Set ySpeed multiplier as a random constant between -1 and 1
-			local ySpeedMultMin = -1
-			local ySpeedMultMax = 1
-			enemyLeft.ySpeedMult = ySpeedMultMin + math.random() * (ySpeedMultMax - ySpeedMultMin)
-			enemyRight.ySpeedMult = ySpeedMultMin + math.random() * (ySpeedMultMax - ySpeedMultMin)
-
-			table.insert(self.enemyTable, enemyLeft)
-			table.insert(self.enemyTable, enemyRight)
-		end
-
-		-- Reduce interval after every spawning
-		self.enemySpawnInterval = self.enemySpawnInterval - 1
-
-		-- If interval is 5, increase spawn count instead and reset interval to original
-		if self.enemySpawnInterval <= 5 then
-			self.enemySpawnCount = self.enemySpawnCount + 1
-			self.enemySpawnInterval = self.originalSpawnInterval
-		end
-
-		self.spawnTimer = self.enemySpawnInterval
+		self:spawnEnemies()
 	end
 	self.spawnTimer = self.spawnTimer - dt
 
-	-- Update projectiles
-	for i = #self.enemyProjectileTable, 1, -1 do
-		local p = self.enemyProjectileTable[i]
-		p:update(dt)
-		if p:shouldDestroy() then
-        	table.remove(self.enemyProjectileTable, i)
-    	end
-    end
+	-- Power up spawning
+	if self.powerUpTimer <= 0 then
+		self:spawnPowerUp()
+	end
+	self.powerUpTimer = self.powerUpTimer - dt
 
-    for i = #self.playerProjectileTable, 1, -1 do
-		local p = self.playerProjectileTable[i]
-		p:update(dt)
-		if p:shouldDestroy() then
-        	table.remove(self.playerProjectileTable, i)
-    	end
-    end
+	self:updateTables(dt)
 
     -- Run collision checks between player projectiles and enemy projectiles
     for _, pp in ipairs(self.playerProjectileTable) do
@@ -167,17 +116,6 @@ function Level:update(dt)
     		end
     	end
     end
-
-    -- Update enemies
-	for i =#self.enemyTable, 1, -1 do
-		local e = self.enemyTable[i]
-		e:update(dt)
-		if e:shouldDestroy() then
-			print("Deleting enemy")
-			io.stdout:flush()
-			table.remove(self.enemyTable, i)
-		end
-	end
 
 	-- Check if player is alive
 	if self.player:shouldDestroy() then self.isOver = true end
@@ -204,18 +142,25 @@ function Level:draw()
 		self:printXCenteredText(("Score: %.0f"):format(self.score), scoreX, scoreY, scoreScale)
 	end
 
-	-- Draw projectiles
+	-- Draw things
 	for _, p in ipairs(self.enemyProjectileTable) do
 		p:draw()
     end
-
     for _, p in ipairs(self.playerProjectileTable) do
 		p:draw()
     end
-
-    -- Draw enemies
 	for _, e in ipairs(self.enemyTable) do
 		e:draw()
+    end
+    for _, pUp in ipairs(self.powerUpTable) do
+		pUp:draw()
+		if pUp.hasBeenPicked then
+			self:printXCenteredText("Power up: " .. pUp.name, 
+				self.powerUpTextCenterX, self.powerUpTextY, self.powerUpTextScale)
+		end
+    end
+    for _, ef in ipairs(self.effectsTable) do
+		ef:draw()
     end
 
     -- Ending
@@ -245,6 +190,127 @@ function Level:draw()
 	end
 end
 
+function Level:spawnEnemies()
+	for i=1, self.enemySpawnCount, 1 do
+		-- Spawn off screen
+		local spawnX1 = -50
+		local spawnX2 = love.graphics.getWidth() + 50
+
+		-- Set random y value for spawn location
+		local spawnY1 = math.random(-100, love.graphics.getHeight() / 2)
+		local spawnY2 = math.random(-100, love.graphics.getHeight() / 2)
+
+		-- Geometry
+		local enemyOrientation = 0
+		local enemyScale = 0.8
+		local enemySize = 64
+
+		-- Inititialize enemies
+		local enemyLeft = Enemy.new(spawnX1, spawnY1, enemyOrientation, 
+			enemyScale, enemyScale, enemySize, enemySize, self.enemyInstructions, self)
+		local enemyRight = Enemy.new(spawnX2, spawnY2, enemyOrientation, 
+			enemyScale, enemyScale, enemySize, enemySize, self.enemyInstructions, self)
+
+		-- Treat current position as supposed center and align accordingly
+		enemyLeft:centerToPos()
+		enemyRight:centerToPos()
+
+		-- Mirror directions in instructions
+		enemyLeft.instructions.mag = 1
+		enemyRight.instructions.mag = -1
+
+		-- Set xSpeed multiplier as a random constant between 0.8 and 1.5
+		local xSpeedMultMin = 0.8
+		local xSpeedMultMax = 1.5
+		enemyLeft.xSpeedMult = xSpeedMultMin + math.random() * (xSpeedMultMax - xSpeedMultMin)
+		enemyRight.xSpeedMult = xSpeedMultMin + math.random() * (xSpeedMultMax - xSpeedMultMin)
+
+		-- Set ySpeed multiplier as a random constant between -1 and 1
+		local ySpeedMultMin = -1
+		local ySpeedMultMax = 1
+		enemyLeft.ySpeedMult = ySpeedMultMin + math.random() * (ySpeedMultMax - ySpeedMultMin)
+		enemyRight.ySpeedMult = ySpeedMultMin + math.random() * (ySpeedMultMax - ySpeedMultMin)
+
+		table.insert(self.enemyTable, enemyLeft)
+		table.insert(self.enemyTable, enemyRight)
+	end
+
+	-- Reduce interval after every spawning
+	self.enemySpawnInterval = self.enemySpawnInterval - 1
+
+	-- If interval is 5, increase spawn count instead, reset interval to original
+	if self.enemySpawnInterval <= 5 then
+		self.enemySpawnCount = self.enemySpawnCount + 1
+		self.enemySpawnInterval = self.originalSpawnInterval
+	end
+
+	self.spawnTimer = self.enemySpawnInterval
+end
+
+function Level:updateTables(dt)
+	-- Update enemy projectiles
+	for i = #self.enemyProjectileTable, 1, -1 do
+		local p = self.enemyProjectileTable[i]
+		p:update(dt)
+		if p:shouldDestroy() then
+        	table.remove(self.enemyProjectileTable, i)
+    	end
+    end
+
+    -- Update player projectiles
+    for i = #self.playerProjectileTable, 1, -1 do
+		local p = self.playerProjectileTable[i]
+		p:update(dt)
+		if p:shouldDestroy() then
+        	table.remove(self.playerProjectileTable, i)
+    	end
+    end
+
+	-- Update enemies
+	for i =#self.enemyTable, 1, -1 do
+		local e = self.enemyTable[i]
+		e:update(dt)
+		if e:shouldDestroy() then
+			print("Deleting enemy")
+			io.stdout:flush()
+			table.remove(self.enemyTable, i)
+		end
+	end
+
+	-- Update power ups
+	for i =#self.powerUpTable, 1, -1 do
+		local pUp = self.powerUpTable[i]
+		pUp:update(dt)
+		if pUp:shouldDestroy() then
+			table.remove(self.powerUpTable, i)
+		end
+	end
+
+	-- Update effects
+	for i =#self.effectsTable, 1, -1 do
+		local ef = self.effectsTable[i]
+		ef:update(dt)
+		if ef:shouldDestroy() then
+			table.remove(self.effectsTable, i)
+		end
+	end
+end
+
+function Level:spawnPowerUp()
+	local spawnX = math.random(150, love.graphics.getWidth() - 150)
+	local spawnY = math.random(150, love.graphics.getHeight() - 150)
+	local powerUpOrientation = 0
+	local powerUpScale = 0.5
+	local powerUpSize = 32
+	table.insert(self.powerUpTable, PowerUp.new(spawnX, spawnY, powerUpOrientation, 
+		powerUpScale, powerUpScale, powerUpSize, powerUpSize, self))
+	self.powerUpTimer = self.powerUpInterval
+end
+
+function Level:spawnEffect(effect)
+	table.insert(self.effectsTable, effect)
+end
+
 function Level:readFile(filename)
 	local tab = {}
 	local contents, err = love.filesystem.read(filename)
@@ -261,7 +327,12 @@ function Level:mousepressed(mx, my, button)
 end
 
 function Level:keypressed(key)
-  	if self.isOver and key == "return" then self.host:setupMainMenu() end
+  	if self.isOver and key == "return" then 
+  		self.host:setupMainMenu()
+  	elseif key == "e" and self.activePowerUp ~= nil then
+  		self.activePowerUp:execute()
+  		self.activePowerUp = nil
+  	end
 end
 
 function Level:printXCenteredText(text, centerX, textY, scale)
